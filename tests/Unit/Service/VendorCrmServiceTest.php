2a49f85ab646b393d1e20d0914c26ff92605f806
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-04-01 13:50:03 -0500

    add vendor crm service tests

---
+++ tests/Unit/Service/VendorCrmServiceTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Service;
+
+use App\Entity\Vendor;
+use App\Service\VendorCrmService;
+use PHPUnit\Framework\TestCase;
+
+final class VendorCrmServiceTest extends TestCase
+{
+    public function testRegisterVendorIsIntentionalNoOpUntilProviderIsConfigured(): void
+    {
+        $vendor = new Vendor('Vendor Example');
+
+        $service = new VendorCrmService();
+        $service->registerVendor($vendor);
+
+        self::assertTrue(true);
+    }
+}
