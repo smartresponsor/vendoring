93e190df84780e21edd569b8c462a227218b3054
OpenAI Local <local@openai.invalid>
2026-04-19 23:16:36 -0500

    stan debug
    ets

--- .native_bundle_backup/20260419-223756/config/bundles.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return array_filter([
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    App\Vendoring\VendoringBundle::class => ['all' => true],
-]);
